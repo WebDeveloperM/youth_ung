e19709c27576330262d9d9d64a04cce0606425e4
evoappcommunity <165270312+evoappcommunity@users.noreply.github.com>
2025-10-06 15:31:06 +0500

    feat: LoginScreen from Raupov

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import { Button } from '@/components/ui/button'
+import React from 'react';
+import { LanguageProvider } from './contexts/LanguageContext';
+import Auth from './components/Auth/Auth';
 
 const App = () => {
 	return (
-		<div className='flex min-h-svh flex-col items-center justify-center'>
-			<Button>Click me</Button>
-		</div>
-	)
-}
+		<LanguageProvider>
+			<Auth />
+		</LanguageProvider>
+	);
+};
 
-export default App
+export default App;
