0dc579be4d57a2ab93e7896d24c9d4e315b258d3
abdirazzoqov <abdirazzoqov@shgkm.akt>
2025-10-07 14:10:25 +0500

    Hero and Footer section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import Footer from './components/footer/Footer'
 import { Header } from './components/header'
+import Hero from './components/hero/Hero'
+
 function App() {
 	return (
 		<section>
 			<Header />
+			<Hero />
+			<Footer />
 		</section>
 	)
 }
