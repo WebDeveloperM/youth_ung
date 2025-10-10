50daf4f02e474620edd0ae2112915c812f8d9942
Shaxzodmalikovgtl <shmalikov@outlook.com>
2025-10-10 11:08:11 +0500

    add: i18n language option

--- frontend/src/components/langSelector/index.jsx
+++ frontend/src/components/langSelector/index.jsx
@@ -1,7 +1,22 @@
+import { useTranslation } from 'react-i18next'
+
 export function LanguageSelector() {
+	const { i18n } = useTranslation()
+
+	const handleChange = e => {
+		const lang = e.target.value
+		i18n.changeLanguage(lang)
+		localStorage.setItem('lang', lang)
+	}
+
 	return (
 		<div>
-			<select className='selectlang' name='selectLang' defaultValue='uz'>
+			<select
+				className='selectlang border rounded p-2'
+				name='selectLang'
+				defaultValue={localStorage.getItem('lang') || 'uz'}
+				onChange={handleChange}
+			>
 				<option value='uz'>Uz</option>
 				<option value='en'>Eng</option>
 				<option value='ru'>Rus</option>
